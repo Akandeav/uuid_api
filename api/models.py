6a6807be97b6a3e7ace111b4aeec70ed09895093
Akande Victor <40690359+Akandeav@users.noreply.github.com>
2022-12-12 17:56:57 +0100

    Bug fix typo

--- api/models.py
+++ api/models.py
@@ -2,5 +2,5 @@ import uuid
 from django.db import models
 
 class UUIDValue(models.Model):
-    time_stamp = models.DateTimeField(verbose_name=_("Creation date"), auto_now_add=True, null=True)
+    time_stamp = models.DateTimeField(verbose_name=("Creation date"), auto_now_add=True, null=True)
     uuid = models.UUIDField(default=uuid.uuid4)
